3aaab08eb0cb0c474796c8f26d4455fec154dc77
Craig Phillips <craig@craigphillips.biz>
2011-02-01 22:49:47 +1000

    Add lightbox view to EasyTable Manager for file uploads.

---
+++ admin/views/easytableupload/tmpl/default.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * @package    EasyTables
+ * @author     Craig Phillips {@link http://www.seepeoplesoftware.com}
+ * @author     Created on 13-Jul-2009
+ */
+
+//--No direct access
+	defined('_JEXEC') or die('Restricted Access');
+	JHTML::_('behavior.tooltip');
+?>
+
+<form action="index.php" method="post" name="adminForm" id="adminForm" enctype="multipart/form-data">
+<div class="col100">
+	<table width="100%">
+		<tr>
+			<td>
+			<fieldset>
+				<div style="float: right">
+					<button type="button" onclick="window.parent.document.getElementById('sbox-window').close();"><?php echo JText::_( 'CLOSE' ); ?></button>
+				</div>
+				<div class="configuration"><?php echo JText::_( 'EASYTABLEPRO' );?> - <?php echo JText::_( 'UPLOAD_DATA' );?></div>
+			</fieldset>
+			<fieldset class="adminform">
+			<legend><?php echo JText::_('UPLOAD_RECORDS_DESC').' \''.$this->row->easytablename.'\''; ?></legend>
+			<table class="adminlist" id="et_uploadData">
+				<tr class="row0">
+					<td width="120" align="right" valign="top" class="key">
+						<label for="easytablename"><span style="font-size: 1.5em;font-weight: bold;"><?php echo JText::_( 'NOTES' ); ?>:</span></label>
+					</td>
+					<td><?php echo JText::_( 'FROM_THIS_DESC' );?></td>
+				</tr>
+				<tr class="row1">
+					<td width="120" align="left" class="key">
+						<label for="uploadType"><span class="hasTip" title="<?php echo JText::_( 'UPLOAD_TYPE_TOOLTIP' );?>"><h3>1) - <?php echo JText::_( 'SELECT_UPLOAD_DESC' ); ?>:</h3></span></label>
+					</td>
+					<td>
+						<input type="radio" name="uploadType" id="uploadType0" value="0" class="inputbox" checked="checked">
+						<label for="uploadType0"><?php echo JText::_( 'REPLACE' ); ?></label>
+						<input type="radio" name="uploadType" id="uploadType1" value="1" class="inputbox">
+						<label for="uploadType1"><?php echo JText::_( 'APPEND' ); ?></label>
+					</td>
+				</tr>
+				<tr class="row0">
+					<td width="120" align="left" class="key">
+						<label for="tableimport"><span class="hasTip" title="<?php echo JText::_( 'UPDATE_FILE_TOOLTIP' );?>"><h3>2) - <?php echo JText::_( 'UPDATE_FILE' ); ?>:</h3></label>
+					</td>
+					<td><!-- MAX_FILE_SIZE must precede the file input field -->
+						<input type="hidden" name="MAX_FILE_SIZE" value="3000000" />
+						<input name="tablefile" type="file" id="fileInputBox" />
+					</td>
+				</tr>
+				<tr class="row1">
+					<td width="120" align="left" class="key"><span class="hasTip" title="<?php echo JText::_( 'CLICK__YE_DESC' );?>"><h3>3) - <?php echo JText::_( 'ST_LINE_DESC' ) ?></h3></span></td>
+					<td><?php echo $this->CSVFileHasHeaders; ?></td>
+				</tr>
+				<tr class="row1">
+					<td width="120" align="left" class="key"><h3>4) - <?php echo JText::_( 'UPLOAD_FILE' ) ?> :</h3></td>
+					<td><input type="button" value="<?php echo JText::_( 'UPLOAD_FILE' ) ?>" onclick="javascript: submitbutton('uploadFile');" /></td>
+				</tr>
+			</table>
+			</fieldset>
+			</td>
+		</tr>
+	</table>
+</div>
+<div class="clr"></div>
+
+<input type="hidden" name="option" value="<?php echo $option; ?>" />
+<input type="hidden" name="id" value="<?php echo $this->row->id; ?>" />
+<input type="hidden" name="task" value="uploadData" />
+<?php echo JHTML::_('form.token'); ?>
+<!-- <input type="hidden" name="controller" value="easytable" /> -->
+</form>
---
+++ admin/views/easytableupload/view.html.php
@@ -0,0 +1,136 @@
+<?php
+/**
+ * @package	   EasyTables
+ * @author	   Craig Phillips {@link http://www.seepeoplesoftware.com}
+ * @author	   Created on 13-Jul-2009
+ */
+
+//--No direct access
+defined('_JEXEC') or die('Restricted Access');
+
+jimport( 'joomla.application.component.view');
+
+/**
+ * HTML View class for the EasyTables Component
+ *
+ * @package	   EasyTables
+ * @subpackage Views
+ */
+
+class EasyTableVieweasytableupload extends JView
+{
+	/**
+	 * View display method
+	 * 
+	 * @return void
+	 **/
+	function display($tpl = null)
+	{
+//		dump('display($tpl = null)','IN: ');
+		
+		//get the document and load the js support file
+		$doc =& JFactory::getDocument();
+		$u = & JURI::getInstance();
+		$doc->addStyleSheet('templates/system/css/system.css');
+
+		//get the EasyTable
+		$row =& JTable::getInstance('EasyTable', 'Table');
+		$cid = JRequest::getVar( 'cid', array(0), '', 'array');
+		$id = $cid[0];
+		$row->load($id);
+
+		// Where do we come from
+		$from = JRequest::getVar( 'from', '' );
+		$default_order_sql = " ORDER BY position;";
+
+		if($from == 'create') {
+			$default_order_sql = " ORDER BY id;";
+		}
+
+		
+		// Get a database object
+		$db =& JFactory::getDBO();
+		if(!$db){
+			JError::raiseError(500,JText::_("COULDN_T_GET_THE_DATABASE_OBJECT_WHILE_GETTING_STATISTICS_FOR_EASYTABLE_ID_").' '.$id);
+		}
+		// Get the meta data for this table
+		$query = "SELECT * FROM ".$db->nameQuote('#__easytables_table_meta')." WHERE easytable_id =".$id.$default_order_sql;
+		$db->setQuery($query);
+		
+		$easytables_table_meta = $db->loadRowList();
+		$ettm_field_count = count($easytables_table_meta);
+
+		// Check for the existence of a matching data table
+		$ettd = FALSE;
+		$ettd_tname = $db->getPrefix().'easytables_table_data_'.$id;
+		$allTables = $db->getTableList();
+
+		$ettd = in_array($ettd_tname, $allTables);
+
+		$state = 'Unpublished';
+
+		if($ettd)
+		{
+			// echo 'Found '.$ettd_tname.' in Table List';
+			// Get the table data for this table
+			$query = "SELECT * FROM ".$db->nameQuote('#__easytables_table_data_'.$id).";";
+			
+			$db->setQuery($query);
+			
+			// Store the table data in a variable
+			$easytables_table_data =$db->loadRowList();
+			$ettd_record_count = count($easytables_table_data);
+
+			if($row->published)
+			{
+				$state = 'Published';
+			}
+		}
+		else
+		{
+			// echo 'Didn\'t find '.$ettd_tname.' in Table List';
+			$easytables_table_data ='';
+			$ettd_record_count = 0;
+			
+			// Make sure that a table with no associated data table is never published
+			$row->published = FALSE;
+			$state = 'Unpublished';
+		}
+		
+
+		// keep the data for the tmpl
+		$this->assignRef('row', $row);
+		if(!$ettd)	// Do not allow it to be published until a table is created.
+		{
+			$this->assignRef('published', JHTML::_('select.booleanlist', 'published', 'class="inputbox" disabled="disabled"', $row->published ));
+		}
+		else
+		{
+			$this->assignRef('published', JHTML::_('select.booleanlist', 'published', 'class="inputbox"', $row->published ));
+		}
+		
+		// Parameters for this table instance
+		$paramsdata = $row->params;
+		$paramsdefs = JPATH_COMPONENT_ADMINISTRATOR.DS.'models'.DS.'easytable.xml';
+		$params = new JParameter( $paramsdata, $paramsdefs );
+		
+		$this->assignRef('params', $params);
+
+		$this->assign('id',$id);		
+		$this->assign('state',$state);
+		$this->assign('createddate', JHTML::date($row->created_));
+		$this->assign('modifieddate', JHTML::date($row->modified_));
+		$this->assignRef('easytables_table_meta',$easytables_table_meta);
+		$this->assign('ettm_field_count',$ettm_field_count);
+		$this->assignRef('ettd',$ettd);
+		$this->assign('ettd_tname',$ettd_tname);
+		$this->assign('CSVFileHasHeaders', JHTML::_('select.booleanlist', 'CSVFileHasHeaders', 'class="inputbox"', 0 ));
+		if($ettd)
+		{
+			$this->assignRef('easytables_table_data',$easytables_table_data);
+			$this->assign('ettd_record_count',$ettd_record_count);
+		}
+
+		parent::display($tpl);
+	}// function
+}// class
