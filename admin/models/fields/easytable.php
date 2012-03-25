a000ceeeedae7f090c57e161bb5b4835486d1448
Craig Phillips <craig@craigphillips.biz>
2012-03-25 23:18:19 +1000

    Convert `easytable` element to a JFormFieldList class.
    Signed-off-by: Craig Phillips <craig@craigphillips.biz>

--- admin/models/fields/easytable.php
+++ admin/models/fields/easytable.php
@@ -5,43 +5,45 @@
  * @license     GNU/GPL http://www.gnu.org/copyleft/gpl.html
  * @author      Craig Phillips {@link http://www.seepeoplesoftware.com}
  */
-	defined('_JEXEC') or die ('Restricted Access');
+defined('_JEXEC') or die ('Restricted Access');
 	
-	class JElementEasyTable extends JElement 
+
+jimport('joomla.html.html');
+jimport('joomla.form.formfield');
+jimport('joomla.form.helper');
+
+class JFormFieldEasyTable extends JFormFieldList
+{
+	/**
+	 * Element name
+	 *
+	 * @access	protected
+	 * @var		string
+	 */
+	public	$type = 'EasyTable';
+
+	protected function getOptions()
 	{
-		/**
-		 * Element name
-		 *
-		 * @access	protected
-		 * @var		string
-		 */
-		var	$_name = 'EasyTable';
-	
-		function fetchElement($name, $value, &$node, $control_name)
-		{
-			$db =& JFactory::getDBO();
-            $result ='';
+		// Initialise variables.
+		$options	= array();
+		$db =& JFactory::getDBO();
+		$result ='';
 
-            if($name = 'id')
-            {
-                $elementQuery = 'SELECT id, easytablename FROM #__easytables WHERE published = 1 ORDER BY easytablename';
+		// Get array of tables to build each option from...
+		$optionsQuery = $db->getQuery(true);
+		$optionsQuery->select('id as value, easytablename as text');
+		$optionsQuery->from('#__easytables');
+		$optionsQuery->where('published = 1');
+		$optionsQuery->orderby('easytablename');
 
-                $db->setQuery($elementQuery);
-                $options = $db->loadObjectList();
-				$noneSelected = array();
-				$noneSelected[] = array('id' => 0,'easytablename' => '-- '.JText::_( 'COM_EASYTABLEPRO_LABEL_NONE_SELECTED' ).' --');
-                array_splice($options,0,0,$noneSelected);
-                
-                $result = JHTML::_('select.genericlist',	$options,							// [array of value/label pairs, ie. the value of the labels shown in the list]
-															$control_name. '[' . $name . ']',	// so that we end up with a params[id] style name for the html control
-															'class="inputbox"',					// optionals attributes of the select control
-															'id',								// the key in the array for the value of list items
-															'easytablename',					// the key in the array for the label used in the select list
-															$value,								// the current value, used to indicate the selected item in the list
-															$control_name . $name				// used for the id of the control?
-									);
-            }
+		$db->setQuery($optionsQuery);
+		$options = $db->loadObjectList();
+		// Don't forget to prefix it with a "None Selected" options
+		$noneSelected = new stdClass();
+		$noneSelected->value = 0;
+		$noneSelected->text = '-- '.JText::_( 'COM_EASYTABLEPRO_LABEL_NONE_SELECTED' ).' --';
+		array_splice($options,0,0,array($noneSelected));
 
-            return $result;
-		}
+		return $options;
 	}
+}
