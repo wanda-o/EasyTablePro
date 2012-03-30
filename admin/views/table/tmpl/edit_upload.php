b798c09cb8c9b105b1d0866dc23bfbd3086d0b77
Craig Phillips <craig@craigphillips.biz>
2012-03-30 14:05:50 +1000

    Fix the layout of the upload panel.
    Changed tooltip to reflect J25 button naming conventions.
    
    Signed-off-by: Craig Phillips <craig@craigphillips.biz>

--- admin/views/table/tmpl/edit_upload.php
+++ admin/views/table/tmpl/edit_upload.php
@@ -11,6 +11,10 @@ defined('_JEXEC') or die('Restricted Access');
 
 <?php echo JHtml::_('sliders.start','upload-sliders', array('useCookie'=>1)); ?>
 	<?php echo JHtml::_('sliders.panel',JText::_('COM_EASYTABLEPRO_UPLOAD_A_DATA_FILE_LEGEND'), 'tableimport-panel'); ?>
+	<div id="uploadWhileModifyingNotice">
+		<p><?php echo JText::_( 'COM_EASYTABLEPRO_TABLE_UPLOAD_DISABLED_TABLE_MODIFIED_MSG' ); ?></p>
+		<p><em><?php echo JText::_( 'COM_EASYTABLEPRO_TABLE_UPLOAD_RE_ENABLE_BY_SAVING_MSG' ); ?></em></p>
+	</div>
 	<fieldset class="adminform" id="tableimport">
 		<!-- MAX_FILE_SIZE must precede the file input field -->
 		<input type="hidden" name="MAX_FILE_SIZE" value="<?php echo $this->maxFileSize ?>" />
@@ -22,18 +26,14 @@ defined('_JEXEC') or die('Restricted Access');
 			}
 		?>:</label><input name="tablefile" type="file" id="fileInputBox" />
 		<?php if($this->item->ettd) {
-			echo '<input type="button" value="'.JText::_( 'COM_EASYTABLEPRO_TABLE_UPLOAD_FILE_BTN' ).'" onclick="javascript: submitbutton(\'updateETDTable\')" id="fileUploadBtn" />';
+			echo '<input type="button" value="'.JText::_( 'COM_EASYTABLEPRO_TABLE_UPLOAD_FILE_BTN' ).'" onclick="javascript: joomla.submitbutton(\'updateETDTable\')" id="fileUploadBtn" />';
 		} else {
-			echo '<input type="button" value="'.JText::_( 'COM_EASYTABLEPRO_TABLE_UPLOAD_FILE_BTN' ).'" onclick="javascript: submitbutton(\'createETDTable\')" id="fileUploadBtn" />';
+			echo '<input type="button" value="'.JText::_( 'COM_EASYTABLEPRO_TABLE_UPLOAD_FILE_BTN' ).'" onclick="javascript: joomla.submitbutton(\'createETDTable\')" id="fileUploadBtn" />';
 		} ?><legend style="clear:both;"><?php echo JText::_( 'COM_EASYTABLEPRO_TABLE_UPLOAD_FILE_HAS_HEADINGS' ); ?></legend>
 		<label for="CSVFileHasHeaders0" id="CSVFileHasHeaders0-lbl" class="radiobtn">No</label>
 		<input type="radio" name="CSVFileHasHeaders" id="CSVFileHasHeaders0" value="0" checked="checked" class="inputbox">
 		<label for="CSVFileHasHeaders1" id="CSVFileHasHeaders1-lbl" class="radiobtn">Yes</label>
 		<input type="radio" name="CSVFileHasHeaders" id="CSVFileHasHeaders1" value="1" class="inputbox">
-		<div id="uploadWhileModifyingNotice">
-			<p><?php echo JText::_( 'COM_EASYTABLEPRO_TABLE_UPLOAD_DISABLED_TABLE_MODIFIED_MSG' ); ?></p>
-			<p><em><?php echo JText::_( 'COM_EASYTABLEPRO_TABLE_UPLOAD_RE_ENABLE_BY_SAVING_MSG' ); ?></em></p>
-		</div>
 	<?php if($this->item->ettd) { // For those uses that manage to save a table without importing data... ?>
 		<legend style="clear:both;"><?php echo JText::_( 'COM_EASYTABLEPRO_TABLE_UPLOAD_INTENTION_TT' );?></legend>
 		<label for="uploadType0"><?php echo JText::_( 'COM_EASYTABLEPRO_TABLE_UPLOAD_REPLACE' ); ?></label>
