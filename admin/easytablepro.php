47764e3f6fc22527ff62b7e8acd06db4bf5093e9
Craig Phillips <craig@craigphillips.biz>
2012-04-08 23:46:36 +1000

    Remote old cppl globals.
    Remove no-longer required registerTask calls.
    
    Signed-off-by: Craig Phillips <craig@craigphillips.biz>

--- admin/easytablepro.php
+++ admin/easytablepro.php
@@ -13,10 +13,6 @@
 //--No direct access
 defined('_JEXEC') or die('Restricted Access');
 
-// define some bits
-define("_cppl_base_com_name","easytable");    // REMEMBER: we can't use defined values in installer obj
-define("_cppl_this_com_name","easytablepro"); // REMEMBER: so you must update install/uninstall manually
-
 // Access check.
 if (!JFactory::getUser()->authorise('core.manage', 'com_easystaging')) {
 	return JError::raiseWarning(404, JText::_('JERROR_ALERTNOAUTHOR'));
@@ -26,32 +22,6 @@ if (!JFactory::getUser()->authorise('core.manage', 'com_easystaging')) {
 jimport('joomla.application.component.controller');
 
 $controller = JController::getInstance('EasyTablePro');
-
-/* Table Manager Fn's */
-$controller->registerTask('unpublish','publish');
-$controller->registerTask('apply', 'save');
-$controller->registerTask('linkTable','linkTable');
-
-/* Data Import Fn's */
-$controller->registerTask('createETDTable', 'save');
-$controller->registerTask('updateETDTable', 'save');
-$controller->registerTask('editdata', 'editData');
-$controller->registerTask('presentUploadScreen', 'presentUploadScreen');
-$controller->registerTask('uploadData', 'uploadData');
-$controller->registerTask('uploadFile', 'uploadData');
-$controller->registerTask('uploadResult', 'uploadResult');
-
-/* Data Edit Fn's */
-$controller->registerTask('addrow', 'addRow');
-$controller->registerTask('deleteRecords', 'deleteRecords');
-$controller->registerTask('editrow', 'editrow');
-$controller->registerTask('saveRecord', 'applyRecord');
-$controller->registerTask('applyRecord', 'applyRecord');
-$controller->registerTask('saveNewRecord', 'applyRecord');
-$controller->registerTask('applyNewRecord', 'applyRecord');
-$controller->registerTask('cancelRecord', 'cancelRecord');
-$controller->registerTask('cancelNewRecord', 'cancelRecord');
-
 // get the task
 $jinput = JFactory::getApplication()->input;
 $controller->execute($jinput->get('task'));
