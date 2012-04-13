fb9166efa72082e388a7858ec6619ce02bb2dfda
Craig Phillips <craig@craigphillips.biz>
2012-04-13 23:51:21 +1000

    First pass at the `upload` view.
    Signed-off-by: Craig Phillips <craig@craigphillips.biz>

--- admin/views/upload/tmpl/default.php
+++ admin/views/upload/tmpl/default.php
@@ -10,19 +10,19 @@
 	JHTML::_('behavior.tooltip');
 ?>
 
-<form action="index.php" method="post" name="adminForm" id="adminForm" enctype="multipart/form-data">
+<form action="index.php?option=com_easytablepro" method="post" name="adminForm" id="adminForm" enctype="multipart/form-data">
 <div class="col100">
 	<table width="100%">
 		<tr>
 			<td>
 			<fieldset>
 				<div style="float: right">
-					<button type="button" onclick="window.parent.document.getElementById('sbox-window').close();"><?php echo JText::_( 'COM_EASYTABLEPRO_LABEL_CLOSE' ); ?></button>
+					<button type="button" onclick="window.parent.SqueezeBox.close();;"><?php echo JText::_( 'COM_EASYTABLEPRO_LABEL_CLOSE' ); ?></button>
 				</div>
 				<div class="configuration"><?php echo JText::_( 'COM_EASYTABLEPRO' );?> - <?php echo JText::_( 'COM_EASYTABLEPRO_MGR_UPLOAD_DATA' );?></div>
 			</fieldset>
 			<fieldset class="adminform">
-			<legend><?php echo JText::_('COM_EASYTABLEPRO_MGR_UPLOAD_RECORDS_DESC').' \''.$this->row->easytablename.'\''; ?></legend>
+			<legend><?php echo JText::_('COM_EASYTABLEPRO_MGR_UPLOAD_RECORDS_DESC').' \'' . $this->form->getValue('easytablename') . '\''; ?></legend>
 			<table class="adminlist" id="et_uploadData">
 				<tr class="row0">
 					<td width="120" align="right" valign="top" class="key">
@@ -32,31 +32,27 @@
 				</tr>
 				<tr class="row1">
 					<td width="120" align="left" class="key">
-						<h3><span class="hasTip" title="<?php echo JText::_( 'COM_EASYTABLEPRO_TABLE_UPLOAD_TYPE_TT' );?>"><label>1) - <?php echo JText::_( 'COM_EASYTABLEPRO_TABLE_UPLOAD_TYPE' ); ?>:</label></span></h3>
+						<h3><span class="hasTip" title="<?php echo JText::_( 'COM_EASYTABLEPRO_TABLE_UPLOAD_TYPE_TT' );?>"><?php echo $this->form->getLabel('uploadType'); ?></span></h3>
 					</td>
 					<td>
-						<input type="radio" name="uploadType" id="uploadType0" value="0" class="inputbox" checked="checked" />
-						<label for="uploadType0"><?php echo JText::_( 'COM_EASYTABLEPRO_TABLE_UPLOAD_REPLACE' ); ?></label>
-						<input type="radio" name="uploadType" id="uploadType1" value="1" class="inputbox" />
-						<label for="uploadType1"><?php echo JText::_( 'COM_EASYTABLEPRO_TABLE_UPLOAD_APPEND' ); ?></label>
+						<?php echo $this->form->getInput('uploadType'); ?>
 					</td>
 				</tr>
 				<tr class="row0">
 					<td width="120" align="left" class="key">
-						<h3><span class="hasTip" title="<?php echo JText::_( 'COM_EASYTABLEPRO_TABLE_UPLOAD_FILE_TT' );?>"><label for="tableimport">2) - <?php echo JText::_( 'COM_EASYTABLEPRO_TABLE_UPLOAD_SELECT_FILE' ); ?>:</label></span></h3>
+						<h3><span class="hasTip" title="<?php echo JText::_( 'COM_EASYTABLEPRO_TABLE_UPLOAD_FILE_TT' );?>"><?php echo $this->form->getLabel('tablefile'); ?></span></h3>
 					</td>
-					<td><fieldset id="tableimport"><!-- MAX_FILE_SIZE must precede the file input field -->
-						<input type="hidden" name="MAX_FILE_SIZE" value="<?php echo $this->maxFileSize ?>" />
-						<input name="tablefile" type="file" id="fileInputBox" /></fieldset>
+					<td>
+						<?php echo $this->form->getInput('tablefile'); ?>
 					</td>
 				</tr>
 				<tr class="row1">
-					<td width="120" align="left" class="key"><h3><span class="hasTip" title="<?php echo JText::_( 'COM_EASYTABLEPRO_MGR_FILE_HEADINGS_DESC' );?>">3) - <?php echo JText::_( 'COM_EASYTABLEPRO_TABLE_CHECKBOX_FILE_HEADINGS' ) ?></span></h3></td>
-					<td><?php echo $this->CSVFileHasHeaders; ?></td>
+					<td width="120" align="left" class="key"><h3><span class="hasTip" title="<?php echo JText::_( 'COM_EASYTABLEPRO_MGR_FILE_HEADINGS_DESC' );?>"><?php echo $this->form->getLabel('CSVFileHasHeaders'); ?></span></h3></td>
+					<td><?php echo $this->form->getInput('CSVFileHasHeaders'); ?></td>
 				</tr>
 				<tr class="row1">
-					<td width="120" align="left" class="key"><h3>4) - <?php echo JText::_( COM_EASYTABLEPRO_TABLE_UPLOAD_FILE_BTN ) ?> :</h3></td>
-					<td><input type="button" value="<?php echo JText::_( COM_EASYTABLEPRO_TABLE_UPLOAD_FILE_BTN ) ?>" onclick="javascript: submitbutton('uploadFile');" /></td>
+					<td width="120" align="left" class="key"><h3><?php echo JText::_( 'COM_EASYTABLEPRO_TABLE_UPLOAD_FILE' ) ?></h3></td>
+					<td><input type="button" value="<?php echo JText::_( 'COM_EASYTABLEPRO_TABLE_UPLOAD_FILE_BTN' ) ?>" onclick="Joomla.submitbutton('upload.uploadFile');" /></td>
 				</tr>
 			</table>
 			</fieldset>
@@ -66,9 +62,6 @@
 </div>
 <div class="clr"></div>
 
-<input type="hidden" name="option" value="<?php echo JRequest::getCmd('option'); ?>" />
-<input type="hidden" name="id" value="<?php echo $this->row->id; ?>" />
-<input type="hidden" name="task" value="uploadData" />
+<input type="hidden" name="task" value="" />
 <?php echo JHTML::_('form.token'); ?>
-<!-- <input type="hidden" name="controller" value="easytable" /> -->
 </form>
--- admin/views/upload/view.html.php
+++ admin/views/upload/view.html.php
@@ -28,117 +28,13 @@ class EasyTableProViewUpload extends JView
 	 **/
 	function display($tpl = null)
 	{
+		$form = $this->get('Form');
+		$item = $this->get('Item');
 		
-		//get the document and load the js support file
-		$doc = JFactory::getDocument();
-		$u = JURI::getInstance();
-		$doc->addScript(JURI::base().'media/com_easytablepro/js/easytableupload.js');
-		$doc->addStyleSheet('templates/system/css/system.css');
-
-		//get the EasyTable
-		$row = JTable::getInstance('EasyTablePro', 'Table');
-		$cid = JRequest::getVar( 'cid', array(0), '', 'array');
-		$id = $cid[0];
-		$row->load($id);
-
-		// Where do we come from
-		$from = JRequest::getVar( 'from', '' );
-		$default_order_sql = " ORDER BY position;";
-
-		if($from == 'create') {
-			$default_order_sql = " ORDER BY id;";
-		}
-
+		// Store it for later
+		$this->form = $form;
+		$this->item = $item;
 		
-		// Get a database object
-		$db = JFactory::getDBO();
-		if(!$db){
-			JError::raiseError(500,JText::_("COM_EASYTABLEPRO_TABLE_GET_STATS_DB_ERROR").' '.$id);
-		}
-		// Get the meta data for this table
-		$query = "SELECT * FROM ".$db->nameQuote('#__easytables_table_meta')." WHERE easytable_id =".$id.$default_order_sql;
-		$db->setQuery($query);
-		
-		$easytables_table_meta = $db->loadRowList();
-		$ettm_field_count = count($easytables_table_meta);
-
-		// Check for the existence of a matching data table
-		$ettd = FALSE;
-		$ettd_tname = $db->getPrefix().'easytables_table_data_'.$id;
-		$allTables = $db->getTableList();
-
-		$ettd = in_array($ettd_tname, $allTables);
-
-		$state = 'Unpublished';
-
-		if($ettd)
-		{
-			// echo 'Found '.$ettd_tname.' in Table List';
-			// Get the table data for this table
-			$query = "SELECT * FROM ".$db->nameQuote('#__easytables_table_data_'.$id).";";
-			
-			$db->setQuery($query);
-			
-			// Store the table data in a variable
-			$easytables_table_data =$db->loadRowList();
-			$ettd_record_count = count($easytables_table_data);
-
-			if($row->published)
-			{
-				$state = 'Published';
-			}
-		}
-		else
-		{
-			// echo 'Didn\'t find '.$ettd_tname.' in Table List';
-			$easytables_table_data ='';
-			$ettd_record_count = 0;
-			
-			// Make sure that a table with no associated data table is never published
-			$row->published = FALSE;
-			$state = 'Unpublished';
-		}
-		
-
-		// keep the data for the tmpl
-		$this->assignRef('row', $row);
-		if(!$ettd)	// Do not allow it to be published until a table is created.
-		{
-			$this->assignRef('published', JHTML::_('select.booleanlist', 'published', 'class="inputbox" disabled="disabled"', $row->published ));
-		}
-		else
-		{
-			$this->assignRef('published', JHTML::_('select.booleanlist', 'published', 'class="inputbox"', $row->published ));
-		}
-		
-		// Parameters for this table instance
-		$paramsdata = $row->params;
-		$paramsdefs = JPATH_COMPONENT_ADMINISTRATOR.'/models/easytable.xml';
-		$params = new JParameter( $paramsdata, $paramsdefs );
-
-		// Get the settings meta record
-		$settings = ET_MgrHelpers::getSettings();
-		// Max file size for uploading
-		$umfs = ET_MgrHelpers::umfs();
-		$maxFileSize = $settings->get('maxFileSize', $umfs); //Get the max file size for uploads from Pref's, default to php settings.
-
-		$this->assignRef('params', $params);
-		$this->assign('maxFileSize', $maxFileSize);
-		$this->assign('id',$id);		
-		$this->assign('state',$state);
-		$this->assign('createddate', JHTML::date($row->created_));
-		$this->assign('modifieddate', JHTML::date($row->modified_));
-		$this->assignRef('easytables_table_meta',$easytables_table_meta);
-		$this->assign('ettm_field_count',$ettm_field_count);
-		$this->assignRef('ettd',$ettd);
-		$this->assign('ettd_tname',$ettd_tname);
-		$this->assign('CSVFileHasHeaders', JHTML::_('select.booleanlist', 'CSVFileHasHeaders', 'class="inputbox"', 0 ));
-		if($ettd)
-		{
-			$this->assignRef('easytables_table_data',$easytables_table_data);
-			$this->assign('ettd_record_count',$ettd_record_count);
-		}
-
 		parent::display($tpl);
 	}// function
 }// class
