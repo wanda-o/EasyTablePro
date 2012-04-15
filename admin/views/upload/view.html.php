f2796ba02f44b7dfc3d75efe53ba7f692c0cfe54
Craig Phillips <craig@craigphillips.biz>
2012-04-15 15:34:45 +1000

    Added system CSS to `upload` view.
    Set the default layout to `upload` to match our renamed layout.
    
    Signed-off-by: Craig Phillips <craig@craigphillips.biz>

--- admin/views/upload/view.html.php
+++ admin/views/upload/view.html.php
@@ -28,12 +28,23 @@ class EasyTableProViewUpload extends JView
 	 **/
 	function display($tpl = null)
 	{
+		//get the document and load the js support file
+
+		$doc = JFactory::getDocument();
+
+		$u = JURI::getInstance();
+
+		$doc->addStyleSheet('templates/system/css/system.css');
+
 		$form = $this->get('Form');
 		$item = $this->get('Item');
 		
 		// Store it for later
 		$this->form = $form;
 		$this->item = $item;
+		
+		// Set our layout
+		$this->setLayout('upload');
 		
 		parent::display($tpl);
 	}// function
