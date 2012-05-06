0c1284948f2b14f9eb081445fb4e4bd1365ffb79
Craig Phillips <craig@craigphillips.biz>
2012-05-06 21:13:55 +1000

    Changed model `table` to `records`.
    Removed all of the legacy stuff in attempt to move to over to a more Joomla! based solution.
    
    Signed-off-by: Craig Phillips <craig@craigphillips.biz>

---
+++ site/models/records.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * @package     EasyTable Pro
+ * @Copyright   Copyright (C) 2012 Craig Phillips Pty Ltd.
+ * @license     GNU/GPL http://www.gnu.org/copyleft/gpl.html
+ * @author      Craig Phillips {@link http://www.seepeoplesoftware.com}
+ */
+
+//--No direct access
+defined('_JEXEC') or die ('Restricted Access');
+
+jimport( 'joomla.application.component.modellist' );
+
+/**
+ * EasyTables Model
+ *
+ * @package    EasyTables
+ * @subpackage Models
+ */
+class EasyTableProModelTable extends JModelList
+{
+	/**
+	 * Model context string.
+	 *
+	 * @var		string
+	 */
+	protected $_context = 'com_easytablepro.table';
+
+	/**
+	 * Method to auto-populate the model state.
+	 *
+	 * Note. Calling getState in this method will result in recursion.
+	 *
+	 * @since	1.6
+	 */
+	protected function populateState()
+	{
+		$app = JFactory::getApplication('site');
+
+		// Load state from the request.
+		$pk = JRequest::getInt('id');
+		$this->setState('table.id', $pk);
+
+		// Get pagination request variables
+		$limit = $jAp->getUserStateFromRequest('global.list.limit', 'limit', $jAp->getCfg('list_limit'), 'int');
+		$limitstart = JRequest::getVar('limitstart', 0, '', 'int');
+		
+		// In case limit has been changed, adjust it
+		$limitstart = ($limit != 0 ? (floor($limitstart / $limit) * $limit) : 0);
+
+		$this->setState('limit', $limit);
+		$this->setState('limitstart', $limitstart);
+
+		// Load the parameters.
+		$params = $app->getParams();
+		$this->setState('params', $params);
+	}
+
+	/**
+	 * Method to get table data.
+	 *
+	 * @param	integer	The id of the article.
+	 *
+	 * @return	mixed	Menu item data object on success, false on failure.
+	 */
+	public function &getItems($pk = null)
+	{
+		// Initialise variables.
+		$pk = (!empty($pk)) ? $pk : (int) $this->getState('table.id');
+		
+		parent::getItems($pk);
+
+		if ($this->cache === null) {
+			$this->cache = array();
+		}
+
+		return $this->cache;
+	}
+	
+	function __construct()
+	{
+		parent::__construct();
+		
+		$jAp = JFactory::getApplication();
+
+		// Get pagination request variables
+		$limit = $jAp->getUserStateFromRequest('global.list.limit', 'limit', $jAp->getCfg('list_limit'), 'int');
+		$limitstart = JRequest::getVar('limitstart', 0, '', 'int');
+		
+		// In case limit has been changed, adjust it
+		$limitstart = ($limit != 0 ? (floor($limitstart / $limit) * $limit) : 0);
+		
+		$this->setState('limit', $limit);
+		$this->setState('limitstart', $limitstart);
+  }
+
+}// class
