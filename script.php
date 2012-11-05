5ab4c799deeaac230f3c701b0fcbecb7a372c189
Craig Phillips <craig@craigphillips.biz>
2012-11-05 11:08:52 +1000

    Metadata for v1.1.1
    Signed-off-by: Craig Phillips <craig@craigphillips.biz>

--- script.php
+++ script.php
@@ -7,7 +7,7 @@ defined('_JEXEC') or die('Restricted access');
  */
 class com_easyTableProInstallerScript
 {
-	public $et_this_version = '1.1.1 (bc1a509)';
+	public $et_this_version = '1.1.1 (7f558f2)';
 
 	/**
 	 * method to install the component
