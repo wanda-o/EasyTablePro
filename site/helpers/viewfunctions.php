06bd4ef0fe354c3aa0a790e3af6e9601855ed576
Craig Phillips <craig@craigphillips.biz>
2012-12-20 01:19:02 +1000

    Add docblocks to methods and class.
    Replaced static calls with self:: rather than ET_VHelper::
    Formatted to Joomla! code sniffer requirements.
    
    Signed-off-by: Craig Phillips <craig@craigphillips.biz>

--- site/helpers/viewfunctions.php
+++ site/helpers/viewfunctions.php
@@ -1,77 +1,129 @@
 <?php
 /**
- * @package     EasyTable Pro
- * @Copyright   Copyright (C) 2012 Craig Phillips Pty Ltd.
- * @license     GNU/GPL http://www.gnu.org/copyleft/gpl.html
- * @author      Craig Phillips {@link http://www.seepeoplesoftware.com}
+ * @package    EasyTable_Pro
+ *
+ * @copyright  Copyright (C) 2012 Craig Phillips Pty Ltd.
+ * @license    GNU/GPL http://www.gnu.org/copyleft/gpl.html
  */
-defined('_JEXEC') or die('Restricted Access');
-	// Get Field With Options
+
+defined('_JEXEC') or die;
+
+/**
+ * ET_VHelper formats field values based on type and field options as set.
+ *
+ * @package  EasyTable_Pro
+ *
+ * @since    1.0
+ */
 class ET_VHelper
 {
-	/*
-		$f = field value
-		$type = user defined type
-		$params = field options
-	*/
-	public static function getFWO ($f='', $type=0, $params=null, $OrigRow, $currentImageDir)
+
+	/**
+	 * Get Field With Options
+	 *
+	 * @param   string      $f                field value
+	 *
+	 * @param   int         $type             defined type
+	 *
+	 * @param   array|null  $params           field options
+	 *
+	 * @param   array       $OrigRow          original row from table
+	 *
+	 * @param   string      $currentImageDir  path to the currently active image directory for this row
+	 *
+	 * @return string   The orignal value after formatting and substitutions.
+	 *
+	 * @since  1.0
+	 **/
+	public static function getFWO ($f, $type, $params, $OrigRow, $currentImageDir)
 	{
 		/* The next line is a work around for a nested foreach bug in early versions of PHP 5.2.x */
-		is_object( $OrigRow ) ? $row = clone $OrigRow : $row = $OrigRow;
+		is_object($OrigRow) ? $row = clone $OrigRow : $row = $OrigRow;
 		/* End of work around */
 
-		if ($f == '') return '';
+		if ($f == '')
+		{
+			return '';
+		}
 
 		$fieldOptions = '';
+
 		if (isset ($params))
 		{
-			$paramsObj = new JRegistry();
+			$paramsObj = new JRegistry;
 			$paramsObj->loadString($params);
-			$rawFieldOptions = $paramsObj->get('fieldoptions','');
+			$rawFieldOptions = $paramsObj->get('fieldoptions', '');
+
 			if (strlen($rawFieldOptions) > 1)
 			{
-				$fieldOptions = pack("H*", substr ( $rawFieldOptions, 1 ));
+				$fieldOptions = pack("H*", substr($rawFieldOptions, 1));
 			}
 		}
 		// Create token array
 		$tokenArray = array ();
-		// @todo don't bother creating the token array if there are no fieldoptions
+
 		if ($fieldOptions != '')
 		{
-			foreach ( $row as $theFieldName => $theFieldValue ) // process the fields that appear in the list view first
+			// Process the fields into an array of equivalent tokens
+			foreach ($row as $theFieldName => $theFieldValue)
 			{
-				 $tokenArray['#'.$theFieldName.'#'] = $theFieldValue;
+				$tokenArray['#' . $theFieldName . '#'] = $theFieldValue;
 			}
 		}
 
-		// Process based on type
-		$fieldWithOptions = ET_VHelper::applyOptions($f, $fieldOptions, $tokenArray);
-		$fieldOptions = ET_VHelper::addOptions($fieldOptions, $tokenArray);  // We process the fieldOptions ready for the different types.
-		switch ($type) {
-			case 0: // text
-				// Nothing needs to be done for a TEXT field type
+		// We process the fieldOptions ready for the different types.
+		$fieldOptions = self::addOptions($fieldOptions, $tokenArray);
+
+		switch ($type)
+		{
+			// Text
+			case 0:
+				$fieldWithOptions = self::applyOptions($f, $fieldOptions, $tokenArray);
 				break;
-			case 1: // image
-				$fieldWithOptions = ET_VHelper::getImageWithOptions($f,$fieldOptions,$row,$currentImageDir);
+
+			// Image
+			case 1:
+				$fieldWithOptions = self::getImageWithOptions($f, $fieldOptions, $currentImageDir);
 				break;
-			case 2: // url
-				$fieldWithOptions = ET_VHelper::getURLWithOptions($f,$fieldOptions,$row);
+
+			// URL
+			case 2:
+				$fieldWithOptions = self::getURLWithOptions($f, $fieldOptions);
 				break;
-			case 3: // mailto
-				$fieldWithOptions = ET_VHelper::getMailWithOptions($f,$fieldOptions,$row);
+
+			// Mailto
+			case 3:
+				$fieldWithOptions = self::getMailWithOptions($f, $fieldOptions);
 				break;
-			case 4: // numbers
-				$fieldWithOptions = ET_VHelper::getNumberWithOptions($f,$fieldOptions,$row);
+
+			// Numbers
+			case 4:
+				$fieldWithOptions = self::getNumberWithOptions($f, $fieldOptions);
 				break;
-			case 5: // date
-				$fieldWithOptions = ET_VHelper::getDateWithOptions($f,$fieldOptions,$row);
+
+			// Date
+			case 5:
+				$fieldWithOptions = self::getDateWithOptions($f, $fieldOptions);
 				break;
-			default: // oh oh we messed up
-				$fieldWithOptions = "<!-- Field Type Error: cellData = $f / cellType = $type / row = ".print_r ( $row,  true ).' -->';
+
+			// Oh oh we messed up
+			default:
+				$fieldWithOptions = "<!-- Field Type Error: cellData = $f / cellType = $type / row = " . print_r($row, true) . ' -->';
 		}
 		return $fieldWithOptions;
 	}
 
+	/**
+	 *  AddOptions replaces all tokens in $fieldOptions with their respective tokens values
+	 *
+	 * @param   string  $fieldOptions  The field options with tokens still in place.
+	 *
+	 * @param   array   $tokenArray    The array('#token#' => 'value') of token values.
+	 *
+	 * @return  string
+	 *
+	 * @since   1.0
+	 */
 	public static function addOptions ($fieldOptions, $tokenArray)
 	{
 		if (empty($fieldOptions))
@@ -80,10 +132,24 @@ class ET_VHelper
 		}
 		else
 		{
-			return strtr ( $fieldOptions, $tokenArray );
+			return strtr($fieldOptions, $tokenArray);
 		}
 	}
 
+	/**
+	 * ApplyOptions simply returns a trimmed version of the field if there are no $fieldOptions
+	 *              otherwise it returns $fieldOptions after all the field tokens have been replaced.
+	 *
+	 * @param   string  $f             The field value.
+	 *
+	 * @param   string  $fieldOptions  The options set for the field.
+	 *
+	 * @param   array   $tokenArray    The array of field values keyed by tokenised field names.
+	 *
+	 * @return  string
+	 *
+	 * @since   1.0
+	 */
 	public static function applyOptions ($f, $fieldOptions, $tokenArray)
 	{
 		if (empty($fieldOptions))
@@ -92,34 +158,58 @@ class ET_VHelper
 		}
 		else
 		{
-			return strtr ( $fieldOptions, $tokenArray );
+			return strtr($fieldOptions, $tokenArray);
 		}
 	}
 
-	public static function getImageWithOptions($f, $fieldOptions, $row, $currentImageDir)
+	/**
+	 * getImageWithOptions expects the field value to be an image name, if there is no image name it simply
+	 *                     places a HTML comment in place of an <img> tag.
+	 *
+	 * @param   string  $f                The field value.
+	 *
+	 * @param   string  $fieldOptions     The field options after tokens have been replaced.
+	 *
+	 * @param   string  $currentImageDir  The path to this tables images directory.
+	 *
+	 * @return  string  Containing <img> tag or <!-- --> comment if no image name provided.
+	 *
+	 * @since   1.0
+	 */
+	public static function getImageWithOptions($f, $fieldOptions, $currentImageDir)
 	{
 		if ($f)
 		{
 			$pathToImage = JURI::root().$currentImageDir.'/'.$f;  // we concatenate the image URL with the tables default image path
 			if ($fieldOptions == '')
 			{
-				$fieldWithOptions = '<img src="'.trim($pathToImage).'" alt="'.$f.'" />';
+				$fieldWithOptions = '<img src="' . trim($pathToImage) . '" alt="' . $f . '" />';
 			}
 			else
 			{
-				$fieldWithOptions = '<img src="'.trim($pathToImage).'" '.$fieldOptions.'alt="'.$f.'" />';
+				$fieldWithOptions = '<img src="' . trim($pathToImage) . '" ' . $fieldOptions . 'alt="' . $f . '" />';
 			}
 		}
 		else
 		{
-			$fieldWithOptions = '<!-- '.JText::_('COM_EASYTABLEPRO_SITE_NO_IMAGE_NAME').' -->';
+			$fieldWithOptions = '<!-- ' . JText::_('COM_EASYTABLEPRO_SITE_NO_IMAGE_NAME') . ' -->';
 		}
 		return $fieldWithOptions;
 	}
 
-	public static function getURLWithOptions($f, $fieldOptions, $row)
+	/**
+	 * getURLWithOptions returns an <a href> wrapped around the field value or the field options,
+	 *                   unless the field contains an <a href> in which case it's just passed back.
+	 *
+	 * @param   string  $f             The field value (i.e. an absolute or relative URL)
+	 *
+	 * @param   string  $fieldOptions  The field options after tokens have been replaced.
+	 *
+	 * @return string
+	 */
+	public static function getURLWithOptions($f, $fieldOptions)
 	{
-		//For fully qualified URL's starting with HTTP we open in a new window, for everything else its the same window.
+		// For fully qualified URL's starting with HTTP(S) we open in a new window, for everything else its the same window.
 		$URLTarget = 'target="_blank"';
 		if (substr($f,0,7)!='http://')
 		{
@@ -127,7 +217,9 @@ class ET_VHelper
 		}
 
 		$fieldWithOptions = '';
-		if (substr($f,0,8)=='<a href=') // Fully formed URL provided by CSV - owners responsibility
+
+		// Check for a fully formed HREF tab provided by CSV - owners responsibility
+		if (substr($f, 0, 8) == '<a href=')
 		{
 			$fieldWithOptions = $f;
 		}
@@ -135,74 +227,113 @@ class ET_VHelper
 		{
 			if (empty($fieldOptions))
 			{
-				$fieldWithOptions = '<a href="'.trim($f).'" '.$URLTarget.'>'.$f.'</a>';
+				$fieldWithOptions = '<a href="' . trim($f) . '" ' . $URLTarget . '>' . $f . '</a>';
 			}
 			else
 			{
-				$fieldWithOptions = '<a href="'.trim($f).'" '.$URLTarget.'>'.$fieldOptions.'</a>';
+				$fieldWithOptions = '<a href="' . trim($f) . '" ' . $URLTarget . '>' . $fieldOptions . '</a>';
 			}
 		}
 
 		return $fieldWithOptions;
 	}
 
-	public static function getMailWithOptions($f, $fieldOptions, $row)
+	/**
+	 * getMailWithOptions cloaks the email address with JHTML's email.cloak function if there are
+	 *                    no field options otherwise it hides the mailto in our home grown cloak
+	 *                    and then wraps that around the field options.
+	 *
+	 * @param   string  $f             The field value.
+	 *
+	 * @param   string  $fieldOptions  The field options after tokens have been replaced.
+	 *
+	 * @return  string
+	 */
+	public static function getMailWithOptions($f, $fieldOptions)
 	{
 		$fieldWithOptions = '';
+
 		if (empty($fieldOptions))
 		{
-			$fieldWithOptions = JHTML::_('Email.cloak',trim($f));
+			$fieldWithOptions = JHTML::_('Email.cloak', trim($f));
 		}
 		else
 		{
-			$emailWrapperStart = '<script language="JavaScript" >OpenMC("'.$fieldOptions.'", ';
+			$emailWrapperStart = '<script language="JavaScript" >OpenMC("' . $fieldOptions . '", ';
 			$emailWrapperEnd = ');</script>';
-			$emailArray = explode( '@', trim($f));
-			$userPartOfEmail = '"'.implode( explode( '.', $emailArray[0]), '", "&#46", "').'", ';  // we convert the parts in JS parameters quotes and periods & @'s turned to HTML entities
-			$domainPartOfEmail = '"'.implode( explode( '.', $emailArray[1]), '", "&#46", "').'"'; // if there's more than one @ we ignore the rest - it's an invalid email anyway.
-			$fieldWithOptions = $emailWrapperStart.$userPartOfEmail.'"&#64", '.$domainPartOfEmail.$emailWrapperEnd;
+			$emailArray = explode('@', trim($f));
+
+			// We convert the parts in JS parameters quotes and periods & @'s turned to HTML entities
+			$userPartOfEmail = '"' . implode(explode('.', $emailArray[0]), '", "&#46", "') . '", ';
+
+			// If there's more than one @ we ignore the rest - it's an invalid email anyway.
+			$domainPartOfEmail = '"' . implode(explode('.', $emailArray[1]), '", "&#46", "') . '"';
+			$fieldWithOptions = $emailWrapperStart . $userPartOfEmail . '"&#64", ' . $domainPartOfEmail . $emailWrapperEnd;
 		}
 
 		return $fieldWithOptions;
 	}
 
-	public static function getNumberWithOptions($f, $fieldOptions, $row)
+	/**
+	 * getNumberWithOptions takes upto 5 options from $fieldOptions, and applies them to the field value,
+	 *                      the 3 original number_format params plus a prefix and a suffix string.
+	 *
+	 * @param   string  $f             The field value.
+	 *
+	 * @param   string  $fieldOptions  The field options after tokens have been replaced.
+	 *
+	 * @return  string
+	 */
+	public static function getNumberWithOptions($f, $fieldOptions)
 	{
 		$fieldWithOptions = '';
+
 		if (empty ($fieldOptions))
 		{
-			$fieldWithOptions = number_format( $f ); // default
+			$fieldWithOptions = number_format($f);
 		}
 		else
 		{
+
 			if (!empty ($f))
 			{
-				$numOptions = explode ( '/', $fieldOptions );
+				// We don't use list($1,$2...)=explode() so that we avoid nasty warning Notices
+				$numOptions = explode('/', $fieldOptions);
 				$number_of_decimals = isset ($numOptions[0]) ? $numOptions[0] : '';
 				$decimal_mark       = isset ($numOptions[1]) ? $numOptions[1] : '';
 				$thousands_mark     = isset ($numOptions[2]) ? $numOptions[2] : '';
 				$number_prefix      = isset ($numOptions[3]) ? $numOptions[3] : '';
-				$number_suffix      = isset ($numOptions[4]) ? $numOptions[4] : '';  // we don't use list($1,$2...)=explode() so that we avoid nasty warning Notices
+				$number_suffix      = isset ($numOptions[4]) ? $numOptions[4] : '';
 
-				$fieldWithOptions = $number_prefix.number_format($f, $number_of_decimals, $decimal_mark, $thousands_mark).$number_suffix;
+				$fieldWithOptions = $number_prefix . number_format($f, $number_of_decimals, $decimal_mark, $thousands_mark) . $number_suffix;
 			}
 		}
 		return $fieldWithOptions;
 	}
 
-	public static function getDateWithOptions($f, $fieldOptions, $row)
+	/**
+	 * getDateWithOptions uses strtotime() to convert the field value to a suitable date string
+	 *
+	 * @param   string  $f             The field value.
+	 *
+	 * @param   string  $fieldOptions  The field options after tokens have been replaced.
+	 *
+	 * @return  string
+	 */
+	public static function getDateWithOptions($f, $fieldOptions)
 	{
 		$fieldWithOptions = '';
-		
+
 		if (($timestamp = strtotime($f)) === false)
 		{
-			$fieldWithOptions = trim($f).'<!-- Not a valid date/time string for `strtotime()` -->';
+			$fieldWithOptions = trim($f) . '<!-- Not a valid date/time string for `strtotime()` -->';
 		}
 		else
 		{
 			if (empty ($fieldOptions))
 			{
-				$fieldWithOptions = date("F j, Y", $timestamp); // default date return
+				// Default date returned
+				$fieldWithOptions = date("F j, Y", $timestamp);
 			}
 			else
 			{
@@ -212,47 +343,114 @@ class ET_VHelper
 		return $fieldWithOptions;
 	}
 
+	/**
+	 * getFieldName is a utility to zip through all the Meta and return their alias'
+	 *
+	 * @param   array   $fields      Field meta array
+	 *
+	 * @param   string  $nameColumn  Meta key to return
+	 *
+	 * @return  array
+	 */
 	public static function getFieldNames( $fields, $nameColumn='fieldalias' )
 	{
 		$fieldNames = array();
-		foreach ($fields as $fieldDetails) {
+
+		foreach ($fields as $fieldDetails)
+		{
 			$fieldNames[] = $fieldDetails[$nameColumn];
 		}
 		return $fieldNames;
 	}
 
+	/**
+	 * getFieldsInListView stub method for getFieldsInView
+	 *
+	 * @param   array  $fieldMeta  The field meta array.
+	 *
+	 * @return  array
+	 */
 	public static function getFieldsInListView( $fieldMeta )
 	{
-		return ET_VHelper::getFieldsInView( $fieldMeta, 'list', true);
+		return self::getFieldsInView( $fieldMeta, 'list', true);
 	}
+
+	/**
+	 * getFieldsInListView stub method for getFieldsInView
+	 *
+	 * @param   array  $fieldMeta  The field meta array.
+	 *
+	 * @return  array
+	 */
 	public static function getFieldsNotInListView( $fieldMeta )
 	{
-		return ET_VHelper::getFieldsInView( $fieldMeta, 'list', false);
+		return self::getFieldsInView( $fieldMeta, 'list', false);
 	}
+
+	/**
+	 * getFieldsInDetailView stub method for getFieldsInView
+	 *
+	 * @param   array  $fieldMeta  The field meta array.
+	 *
+	 * @return  array
+	 */
 	public static function getFieldsInDetailView( $fieldMeta )
 	{
-		return ET_VHelper::getFieldsInView( $fieldMeta, 'detail', true);
+		return self::getFieldsInView( $fieldMeta, 'detail', true);
 	}
+
+	/**
+	 * getFieldsNotInDetailView stub method for getFieldsInView
+	 *
+	 * @param   array  $fieldMeta  The field meta array.
+	 *
+	 * @return  array
+	 */
 	public static function getFieldsNotInDetailView( $fieldMeta )
 	{
-		return ET_VHelper::getFieldsInView( $fieldMeta, 'detail', false);
+		return self::getFieldsInView( $fieldMeta, 'detail', false);
 	}
+
+	/**
+	 * getFieldsInView returns fields of the specified $view that are visible or not.
+	 *
+	 * @param   array   $fieldMeta  The field meta array.
+	 *
+	 * @param   string  $view       The view type 'list' or 'detail'
+	 *
+	 * @param   bool    $inOrOut    The visible in view flag.
+	 *
+	 * @return  array
+	 */
 	public static function getFieldsInView( $fieldMeta, $view='list', $inOrOut=true)
 	{
 		$matchedFields = array();
-		foreach ($fieldMeta as $theField) {
-			if ($theField[$view.'_view'] == $inOrOut)
+
+		foreach ($fieldMeta as $theField)
+		{
+			if ($theField[$view . '_view'] == $inOrOut)
+			{
 				$matchedFields[] = $theField;
+			}
 		}
 		return $matchedFields;
 	}
 
+	/**
+	 * hasEmailtype checks the supplied array of field meta to see if any are of type 'mail' i.e. 3
+	 *
+	 * @param   array  $metaArray  The field meta array.
+	 *
+	 * @return bool
+	 */
 	public static function hasEmailType ($metaArray)
 	{
 		foreach ( $metaArray as $metaRecordArray )
 		{
-		    if ($metaRecordArray['type'] == 3)
+			if ($metaRecordArray['type'] == 3)
+			{
 				return true;
+			}
 		}
 		return false;
 	}
