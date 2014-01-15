970df019f6ef9c3ecc61d21afa629b57009e2a19
Craig Phillips <craig@craigphillips.biz>
2014-01-16 00:25:35 +1100

    Fix undefined function error
    Fixed JSLint issues.
    
    Signed-off-by: Craig Phillips <craig@craigphillips.biz>

--- media/js/jquery/dataTables.currencyTypeDetect.js
+++ media/js/jquery/dataTables.currencyTypeDetect.js
@@ -1,23 +1,20 @@
-(function(){
- 
-// Change this list to the valid characters you want
-var validChars = "$£€c¥" + "0123456789" + ".-,'";
- 
-// Init the regex just once for speed - it is "closure locked"
-var
-    str = jQuery.fn.dataTableExt.oApi._fnEscapeRegex( validChars ),
-    re = new RegExp('[^'+str+']');
- 
- 
-jQuery.fn.dataTableExt.aTypes.unshift(
-   function ( data )
-    {
-        if ( typeof data !== 'string' || re.test(data) ) {
-            return null;
+/*jslint nomen: true */
+/*global jQuery*/
+(function () {
+    'use strict';
+    // Change this list to the valid characters you want
+    var validChars = '$£€c' + '0123456789' + ".-,'",
+        str = jQuery.fn.dataTableExt.oApi._fnEscapeRegex(validChars),
+        re = new RegExp('[^' + str + ']');
+
+    jQuery.fn.dataTableExt.aTypes.unshift(
+        function (data) {
+            if (typeof data !== 'string' || re.test(data)) {
+                return null;
+            }
+
+            return 'currency';
         }
- 
-        return 'currency';
-    }
-);
- 
-}());
+    );
+
+});
