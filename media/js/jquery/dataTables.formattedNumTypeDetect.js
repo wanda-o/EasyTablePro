d998eb70b7dc14bfb616c4cd6ad3dc1a33d53b95
Craig Phillips <craig@craigphillips.biz>
2014-01-16 00:25:51 +1100

    Fix JSLint issues.
    Signed-off-by: Craig Phillips <craig@craigphillips.biz>

--- media/js/jquery/dataTables.formattedNumTypeDetect.js
+++ media/js/jquery/dataTables.formattedNumTypeDetect.js
@@ -1,8 +1,9 @@
-jQuery.fn.dataTableExt.aTypes.unshift( 
-    function ( sData )
-    {
-        var deformatted = sData.replace(/[^\d\-\.\/a-zA-Z]/g,'');
-        if ( $.isNumeric( deformatted ) || deformatted === "-" ) {
+/*jslint nomen: true */
+/*global jQuery*/
+jQuery.fn.dataTableExt.aTypes.unshift(
+    function (sData) {
+        var deformatted = sData.replace(/[^\d\-\.\/a-zA-Z]/g, '');
+        if (jQuery.isNumeric(deformatted) || deformatted === "-") {
             return 'formatted-num';
         }
         return null;
