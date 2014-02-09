5793f1cc3f9b03bec010765e03acc94f44740177
Craig Phillips <craig@craigphillips.biz>
2014-02-09 21:55:09 +1100

    Fix JSLint issues.
    Signed-off-by: Craig Phillips <craig@craigphillips.biz>

--- media/js/easytablelink.js
+++ media/js/easytablelink.js
@@ -5,30 +5,30 @@
  * @author      Craig Phillips {@link http://www.seepeoplesoftware.com}
 */
 
-com_EasyTablePro.Link.selectTable = function ()
-{
-	var et_table_select_form = document.adminForm;
-	if (et_selectedTableName = this.checkTableSelection())
-	{
-		et_table_select_form.submit();
-		return 1;
-	}
-}
+com_EasyTablePro.Link.selectTable = function () {
+    'use strict';
+    var et_table_select_form = document.adminForm;
+    var et_selectedTableName = this.checkTableSelection();
+    if (et_selectedTableName)
+    {
+        et_table_select_form.submit();
+        return 1;
+    }
+};
 
-com_EasyTablePro.Link.checkTableSelection = function ()
-{
-	var et_aTableIsSelected = $('tablesForLinking').value;
-	if (et_aTableIsSelected != 0)
-	{
-		return et_aTableIsSelected;
-	}
+com_EasyTablePro.Link.checkTableSelection = function () {
+    'use strict';
+    var et_aTableIsSelected = document.getElementById('tablesForLinking').value;
+    if (et_aTableIsSelected !== 0)
+    {
+        return et_aTableIsSelected;
+    }
 
-	alert(Joomla.JText._('COM_EASYTABLEPRO_DATA_JS_PLEASE_SELECT_A_TABLE'));
-	return 0;
-}
+    alert(Joomla.JText._('COM_EASYTABLEPRO_DATA_JS_PLEASE_SELECT_A_TABLE'));
+    return 0;
+};
 
-com_EasyTablePro.Link.editTable = function ()
-{
-	window.top.location = 'index.php?option=com_easytablepro&task=table.edit&id='+com_EasyTablePro.Tools.getID();
-}
-
+com_EasyTablePro.Link.editTable = function () {
+    'use strict';
+    window.top.location = 'index.php?option=com_easytablepro&task=table.edit&id='+com_EasyTablePro.Tools.getID();
+};
