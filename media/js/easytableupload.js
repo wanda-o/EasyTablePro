fb6547a983c12ddadb18d3dc2f1e815eefeb94af
Craig Phillips <craig@craigphillips.biz>
2014-06-26 21:30:54 +1000

    Fix upload screen's Joomla.submitButton() check.
    Signed-off-by: Craig Phillips <craig@craigphillips.biz>

--- media/js/easytableupload.js
+++ media/js/easytableupload.js
@@ -5,19 +5,21 @@
  * @author      Craig Phillips {@link http://www.seepeoplesoftware.com}
 */
 
-function Joomla.submitbutton(pressbutton)
+Joomla.submitbutton = function (pressbutton)
 {
-	if (pressbutton == 'uploadFile')
+    "use strict";
+
+	if (pressbutton === 'uploadFile')
 	{
 		var tFileName = document.adminForm.tablefile.value;
-		if (tFileName == '')
+		if (tFileName === '')
 		{
 			alert(Joomla.JText._('COM_EASYTABLEPRO_DATA_JS_PLEASE_CHOOSE_A_FILE'));
 			return 0;
 		}
 		
 		var dot = tFileName.lastIndexOf(".");
-		if (dot == -1)
+		if (dot === -1)
 		{
 			alert (Joomla.JText._('COM_EASYTABLEPRO_UPLOAD_JS_ONLY_FILES_WITH_A_CSV_OR_TAB_EXTENSION_ARE_SUPPORTED_NO_EXTENSION_FOUND'));
 			return 0;
@@ -26,7 +28,7 @@ function Joomla.submitbutton(pressbutton)
 		var tFileExt = tFileName.substr(dot,tFileName.length);
 		tFileExt = tFileExt.toLowerCase();
 
-		if ((tFileExt != ".csv") && (tFileExt != ".tsv"))
+		if ((tFileExt !== ".csv") && (tFileExt !== ".tsv"))
 		{
 			alert (com_EasyTablePro.Tools.sprintf(Joomla.JText._('COM_EASYTABLEPRO_UPLOAD_JS_ONLY_FILES_WITH_AN_EXTENSION_OF_CSV_OR_TAB_ARE_SUPPORTED_FOUND_X'), tFileExt));
 			return 0;
