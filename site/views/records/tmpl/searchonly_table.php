d01be83889b6214728da6eaa11892dc6eb5af97b
Craig Phillips <craig@craigphillips.biz>
2014-06-27 21:13:31 +1000

    Fix missing _JEXEC
    Fix whitespace issues.
    
    Signed-off-by: Craig Phillips <craig@craigphillips.biz>

--- site/views/records/tmpl/searchonly_table.php
+++ site/views/records/tmpl/searchonly_table.php
@@ -1,25 +1,3 @@
-		if ($this->show_pagination_header && !$this->show_pagination_footer)
-		{
-			// Only if pagination is enabled
-			if ($this->show_pagination && $this->etmCount)
-			{
-				echo '<div class="pagination">';
-				echo $this->pagination->getListFooter();
-				echo '</div>';
-			}
-		}
-		?>
-		<table id="<?php echo htmlspecialchars($this->easytable->easytablealias); ?>" summary="<?php echo htmlspecialchars(strip_tags($this->easytable->description)); ?>" width="100%">
-			<thead>
-			<tr>
-				<?php
-				$headingCount = 0;
-				foreach ($this->easytables_table_meta as $heading )
-				{
-					if (!$heading['list_view'])
-					{
-						continue;
-					}
 <?php
 /**
  * @package    EasyTable_Pro
@@ -29,43 +7,68 @@
  * @url        http://www.seepeoplesoftware.com
  */
 
-					$titleString = '';
+defined('_JEXEC') or die ('Restricted Access');
 
-					if (strlen($heading['description']))
-					{
-						$titleString = 'class="hasTip" title="' . htmlspecialchars($heading['description']) . '" ';
-					}
+if ($this->show_pagination_header && !$this->show_pagination_footer)
+{
+    // Only if pagination is enabled
+    if ($this->show_pagination && $this->etmCount)
+    {
+        echo '<div class="pagination">';
+        echo $this->pagination->getListFooter();
+        echo '</div>';
+    }
+}
+?>
+<table id="<?php echo htmlspecialchars($this->easytable->easytablealias); ?>" summary="<?php echo htmlspecialchars(strip_tags($this->easytable->description)); ?>" width="100%">
+    <thead>
+    <tr>
+        <?php
+        $headingCount = 0;
+        foreach ($this->easytables_table_meta as $heading )
+        {
+            if (!$heading['list_view'])
+            {
+                continue;
+            }
 
-					$headingClass = 'sectiontableheader ' . $heading['fieldalias'];
-					echo '<th class="' . $headingClass . '" ><span ' . $titleString . ' >' . $heading['label'] . '</span></th>';
-					$headingCount++;
-				}
-				?>
-			</tr>
-			</thead>
-			<?php
-			if ($this->itemCount)
-			{
-				echo $this->loadTemplate('tablebody');
-			}
-			else
-			{
-				echo "<tbody><tr id='et_no_records'><td colspan='$headingCount'>$this->noResultsMsg</td></tr></tbody>";
-			}
-			?>
-		</table>
-		<?php
-		if ($this->SortableTable)
-		{ ?>
-			<script type="text/javascript">
-				var t = new SortableTable(document.getElementById('<?php echo htmlspecialchars($this->easytable->easytablealias); ?>'), 'etAscending', 'etDescending');
-			</script>
-		<?php
-		}
-		if ($this->show_pagination && $this->show_pagination_footer && $this->etmCount && $this->itemCount) // If pagination is enabled show the controls
-		{
-			echo '<div class="pagination">';
-			echo $this->pagination->getListFooter();
-			echo '</div>';
-		}
-		?>
+            $titleString = '';
+
+            if (strlen($heading['description']))
+            {
+                $titleString = 'class="hasTip" title="' . htmlspecialchars($heading['description']) . '" ';
+            }
+
+            $headingClass = 'sectiontableheader ' . $heading['fieldalias'];
+            echo '<th class="' . $headingClass . '" ><span ' . $titleString . ' >' . $heading['label'] . '</span></th>';
+            $headingCount++;
+        }
+        ?>
+    </tr>
+    </thead>
+    <?php
+    if ($this->itemCount)
+    {
+        echo $this->loadTemplate('tablebody');
+    }
+    else
+    {
+        echo "<tbody><tr id='et_no_records'><td colspan='$headingCount'>$this->noResultsMsg</td></tr></tbody>";
+    }
+    ?>
+</table>
+<?php
+if ($this->SortableTable)
+{ ?>
+    <script type="text/javascript">
+        var t = new SortableTable(document.getElementById('<?php echo htmlspecialchars($this->easytable->easytablealias); ?>'), 'etAscending', 'etDescending');
+    </script>
+<?php
+}
+if ($this->show_pagination && $this->show_pagination_footer && $this->etmCount && $this->itemCount) // If pagination is enabled show the controls
+{
+    echo '<div class="pagination">';
+    echo $this->pagination->getListFooter();
+    echo '</div>';
+}
+?>
