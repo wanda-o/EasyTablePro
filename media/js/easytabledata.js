3c25054766f9bb5990b3d5f9615b3a05151a9d4f
Craig Phillips <craig@craigphillips.biz>
2014-07-06 13:48:33 +1000

    Fix JSHint issues.
    Signed-off-by: Craig Phillips <craig@craigphillips.biz>

--- media/js/easytabledata.js
+++ media/js/easytabledata.js
@@ -8,45 +8,49 @@
 
 
 
-et_check_msg = '';
-et_give_delete_warning = true;
+var et_check_msg = '';
+var et_give_delete_warning = true;
 
 function submitbutton(pressbutton)
 {
+    "use strict";
 	return Joomla.submitbutton (pressbutton);
 }
 
 Joomla.submitbutton= function (pressbutton)
 {
-	if (pressbutton == 'records.delete') {
+    "use strict";
+	if (pressbutton === 'records.delete') {
 		if (et_give_delete_warning) {
 			et_give_delete_warning = false;
-			et_deleteThisRecord = confirm(Joomla.JText._('COM_EASYTABLEPRO_DATA_JS_THIS_ACTION_WILL_DELETE_RECORD'));
+			var et_deleteThisRecord = confirm(Joomla.JText._('COM_EASYTABLEPRO_DATA_JS_THIS_ACTION_WILL_DELETE_RECORD'));
 			if (et_deleteThisRecord) {
 				submitform(pressbutton);
 			}
 			else
-				return 0;
+            {
+                return 0;
+            }
 		}
 	}
 	else
 	{
 		Joomla.submitform(pressbutton);
 	}
-}
+};
 
 com_EasyTablePro.pop_Image = function (theURL, theImageElement)
 {
-	theImage = document.getElementById(theImageElement);
-	theHeight = theImage.naturalHeight;
-	if (theHeight != 'undefined')
+    "use strict";
+	var theImage = document.getElementById(theImageElement);
+	var theHeight = theImage.naturalHeight;
+    var theWidth = theImage.naturalWidth;
+	if (typeof theHeight === 'undefined')
 	{
-		theWidth = theImage.naturalWidth;
-	} else {
-		theHeight = 400;
+        theHeight = 400;
 		theWidth = 400;
 	}
-	theWindowConfig = 'height=' + theHeight + ', width=' + theWidth + ', toolbar=no, menubar=no, scrollbars=no, resizable=yes, location=no, directories=no, status=no'
-	window.open (theURL, 'newwindow', config=theWindowConfig)
+	var theWindowConfig = 'height=' + theHeight + ', width=' + theWidth + ', toolbar=no, menubar=no, scrollbars=no, resizable=yes, location=no, directories=no, status=no';
+	window.open (theURL, 'newwindow', theWindowConfig);
 
-}
+};
