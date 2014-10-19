965e77970f3e893c61c12780acfefaa9ad9eda03
Craig Phillips <craig@craigphillips.biz>
2014-10-20 10:54:03 +1100

    Add multi-file import support to CLI
    Signed-off-by: Craig Phillips <craig@craigphillips.biz>

--- cli/easytablespro_import_cron.php
+++ cli/easytablespro_import_cron.php
@@ -8,8 +8,22 @@
 
 /**
  * This is a CRON script which should be called from the command-line, not the
- * web. For example something like:
- * /usr/bin/php /path/to/site/cli/easytablespro_import_cron.php --tableId=1 --filepath=/path/to/file.csv
+ * web. There are two use modes, per file commands, for example something like:
+ * /usr/bin/php /path/to/site/cli/easytablespro_import_cron.php --tableId=1 --filePath=/path/to/file.csv
+ * 
+ * The second use mode is for multiple file imports from a directory, like this:
+ * /usr/bin/php /path/to/site/cli/easytablespro_import_cron.php --sourceDir=/path/to/dir
+ * 
+ * If --sourceDir is specified then --tableId and --filePath are ignored. When --sourceDir is specified
+ * all optional parameters described below will be applied to all files. Files in the source directory
+ * must follow the naming convention tableId.csv or tableId.tsv, where tableId is the numeric value of the table.
+ *
+ * e.g. valid file names would be 1.csv or 12.tsv where 1 and 12 correspond to table id's shown in
+ * the EasyTable Pro Table Manager view of the component.
+ *
+ * For multiple uploads to the same table the extended naming convention can be used, this takes the form:
+ * tableId-string.csv OR tableId-A_long_string.tsv OR tableId-A-very-long_string.tsv
+ * 
  * Optional parameters are 'fileHasHeaders', 'append' and 'deleteFile'.
  *
  * If 'fileHasHeaders=0' the first line of the file is treated as a record, otherwise,
@@ -95,51 +109,96 @@ class EasyTablePro_TableUpdate extends JApplicationCli
 
 			// Get the parameters of this run
 			$chunkSize = $params->get('chunkSize', 50);
-			$tableId = $this->input->getInt('tableId', 0);
-			$filepath = $this->input->getString('filePath', '');
+
+			// Get the CRON parameters, starting with sourceDir
+            $sourceDir = $this->input->getString('sourceDir', '');
+            $sourceFiles = array();
+
+            if ($sourceDir == '')
+            {
+                $aTableId = $this->input->getInt('tableId', 0);
+                $aFilepath = $this->input->getString('filePath', '');
+                $sourceFiles[] = array(
+                    'tableId' => $aTableId,
+                    'filePath' => $aFilepath,
+                );
+            }
+            elseif (is_dir($sourceDir))
+            {
+                $sourceFiles = $this->processSourceDirectory($sourceDir);
+            }
+            else
+            {
+                $this->outf('Source Directory ( %s ) is not a directory. EXITING.', array($sourceDir));
+            }
+
+            // Get any optional settings
 			$fileHasHeaders = $this->input->getInt('fileHasHeaders', 1);
 			$append = $this->input->getInt('append', 0);
 			$deleteFile = $this->input->getInt('deleteFile', 1);
 
-			if ($tableId && $filepath)
-			{
-				// and inject them into our model…
-				$result = $uploadModel->cronUpdate($tableId,
-					$filepath,
-					$fileHasHeaders,
-					$chunkSize,
-					$append,
-					$db,
-					$this);
-			}
-			else
-			{
-				$this->outf('Usage: tableId and filePath must be specified.');
-				$result = 0;
-			}
+            // Process our files
+            $filesProcessed = $this->processSourceFiles($sourceFiles, $uploadModel, $db, $chunkSize,
+                $fileHasHeaders, $append, $deleteFile);
+            if ($filesProcessed == 1)
+            {
+                $this->outf('CRON Import processed one (1) file.');
+            }
+            elseif ($filesProcessed > 1)
+            {
+                $this->outf('CRON Import processed %s files.', array($filesProcessed));
+            }
+            else
+            {
+                $this->outf('CRON found no VALID files to process.');
+            }
 
-			if ($result)
-			{
-				$this->outf('Update added %s records.', $result);
-				// Clean up the datafile?
-				if ($deleteFile)
-				{
-					unlink($filepath);
-				}
-
-			}
-			else
-			{
-				$this->outf('CRON Update DID NOT add any records');
-			}
-
-			$this->outf('EasyTable Pro! CRON Import Finished');
+            $this->outf('EasyTable Pro! CRON Import Finished');
 		}
 		else
 		{
 			$this->outf('EasyTable Pro! regrets to inform you that CRON Imports are not enabled. Have a nice day!');
 		}
 	}
+
+    private function processSourceDirectory($sourceDir)
+    {
+        $files = scandir($sourceDir);
+        $sourceFiles = array();
+
+        if (substr($sourceDir, -1) != '/')
+        {
+            $sourceDir .= '/';
+        }
+
+        foreach ($files as $possibleFile)
+        {
+            // We don't traverse up of down from the supplied directory
+            if (is_dir($possibleFile))
+            {
+                continue;
+            }
+
+            // So what about our files details
+            $ext = pathinfo($possibleFile, PATHINFO_EXTENSION);
+            $name = pathinfo($possibleFile, PATHINFO_FILENAME);
+
+            $nameParts = explode('-', $name);
+            $tableId = $nameParts[0];
+
+            // If it's a correctly named CSV/TSV we collect it
+            if (($ext == 'csv' || $ext == 'tsv') && is_numeric($tableId))
+            {
+                $sourceFiles[] = array(
+                    'tableId' => $tableId,
+                    'filePath' => $sourceDir.$possibleFile,
+                );
+            }
+        }
+
+
+        return $sourceFiles;
+    }
 
     /**
      * Produces formatted output via vsprintf and our line length conformer.
@@ -288,6 +347,62 @@ class EasyTablePro_TableUpdate extends JApplicationCli
                 $currentLine = $subPart;
             }
         }
+    }
+
+    /**
+     * Process an array of source files and uploads them to EasyTable Pro tables.
+     *
+     * @param   array                    $sourceFiles  Array of tableId's and filePaths
+     * @param   EasyTableProModelUpload  $uploadModel  The upload model.
+     * @param   JDatabaseDriver          $db           The DB instance to use.
+     * @param   int                      $chunkSize
+     * @param   int                      $fileHasHeaders
+     * @param   int                      $append
+     * @param   int                      $deleteFile
+     *
+     * @return  int
+     */
+    private function processSourceFiles($sourceFiles, $uploadModel, $db, $chunkSize = 50, $fileHasHeaders = 1, $append = 0, $deleteFile = 1)
+    {
+        if (count($sourceFiles))
+        {
+            $filesProcessed = 0;
+
+            // Loop through our source files
+            foreach ($sourceFiles as $fileDetails)
+            {
+                $tableId = $fileDetails['tableId'];
+                $filePath = $fileDetails['filePath'];
+
+                if ($tableId && $filePath) {
+                    // and inject them into our model…
+                    $result = $uploadModel->cronUpdate($tableId,
+                        $filePath,
+                        $fileHasHeaders,
+                        $chunkSize,
+                        $append,
+                        $db,
+                        $this);
+                } else {
+                    $this->outf('Usage: tableId and filePath must be specified.');
+                    $result = 0;
+                }
+
+                if ($result) {
+                    $filesProcessed++;
+                    $this->outf('Update added %s records.', array($result));
+                    // Clean up the datafile?
+                    if ($deleteFile) {
+                        unlink($filePath);
+                    }
+
+                } else {
+                    $this->outf('CRON Update DID NOT add any records in table ID: %s', array($tableId));
+                }
+            }
+        }
+
+        return $filesProcessed;
     }
 }
 
