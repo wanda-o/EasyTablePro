54879cad7945210714577ccc2e05624cbe43cb19
Craig Phillips <craig@craigphillips.biz>
2015-08-05 14:45:32 +1000

    Fix PSR-1/2 issues from PHPCS

--- site/router.php
+++ site/router.php
@@ -22,75 +22,56 @@ function easyTableProBuildRoute(&$query)
 {
     $segments = array();
 
-    if (isset($query['view']))
-    {
+    if (isset($query['view'])) {
         // Store the view for later
         $targetView = $query['view'];
         $targetID   = isset($query['id'])?$query['id']:0;
 
         // We need a few things, so, get a menu item based on Itemid or currently active
-        $app        = JFactory::getApplication();
-        $db         = JFactory::getDbo();
-        $menu       = $app->getMenu();
+        $app  = JFactory::getApplication();
+        $db   = JFactory::getDbo();
+        $menu = $app->getMenu();
 
         // We need a menu item.  Either the one specified in the query, or the current active one if none specified
-        if (empty($query['Itemid']))
-        {
+        if (empty($query['Itemid'])) {
             $menuItem = $menu->getActive();
-        }
-        else
-        {
+        } else {
             $menuItem = $menu->getItem($query['Itemid']);
         }
 
         // Get the current menuItem's settings
-        if (isset($menuItem->query['view']))
-        {
+        if (isset($menuItem->query['view'])) {
             $menusCurrentView = $menuItem->query['view'];
-        }
-        else
-        {
+        } else {
             $menusCurrentView = '';
         }
 
         // Get the menuItem's current component
-        if (isset($menuItem->query['option']))
-        {
+        if (isset($menuItem->query['option'])) {
             $menusCurrentOption = $menuItem->query['option'];
-        }
-        else
-        {
+        } else {
             $menusCurrentOption = '';
         }
 
         // If we have an option it's likely we have a data ID
-        if ($menusCurrentOption && isset($menuItem->query['id']))
-        {
+        if ($menusCurrentOption && isset($menuItem->query['id'])) {
             $menusCurrentID = $menuItem->query['id'];
-        }
-        else
-        {
+        } else {
             $menusCurrentID = 0;
         }
 
         unset($query['view']);
 
-        switch ($targetView)
-        {
+        switch ($targetView) {
             case 'records':
                 // We need the id of the target table, otherwise it's all moot.
-                if (isset($query['id']))
-                {
+                if (isset($query['id'])) {
                     // If we're in a `records` view is the target within the same table?
-                    if ($menusCurrentView == 'records' && $targetID == $menusCurrentID)
-                    {
+                    if ($menusCurrentView == 'records' && $targetID == $menusCurrentID) {
                         unset($query['id']);
-                    }
-                    else
-                    {
+                    } else {
                         // OK we may need to convert a numeric id to an alias id
-                        if (is_numeric($targetID))
-                        {
+                        if (is_numeric($targetID)) {
                             $SQLquery = $db->getQuery(true);
 
                             // Search for the alias of the table of this id
@@ -101,29 +82,25 @@ function easyTableProBuildRoute(&$query)
                             $idAlias = $db->loadResult();
 
                             // Set the next segment to the alias
-                        }
-                        else
-                        {
+                        } else {
                             $idAlias = $targetID;
                         }
 
                         // Make sure we have a result.
-                        if ($idAlias != '')
-                        {
+                        if ($idAlias != '') {
                             unset($query['id']);
                             $segments[] = $idAlias;
                         }
                     }
                 }
-            break;
+                break;
 
             // Link to Record Detail view requested
             case 'record':
-                if (isset($query['id']))
-                {
+                if (isset($query['id'])) {
                     // So, is our current view already showing the table of the target URL?
-                    if ((($menusCurrentOption == 'com_easytablepro') && ($targetID != $menusCurrentID)) || $menusCurrentOption == '')
-                    {
+                    if ((($menusCurrentOption == 'com_easytablepro') &&
+                            ($targetID != $menusCurrentID)) || $menusCurrentOption == '') {
                         // We'll need to add an alias to point to the right table...
                         $SQLquery = $db->getQuery(true);
 
@@ -139,30 +116,25 @@ function easyTableProBuildRoute(&$query)
                     }
 
                     unset($query['id']);
-                }
-                else
-                {
+                } else {
                     return array();
                 }
 
-                if (isset($query['rid']))
-                {
+                if (isset($query['rid'])) {
                     $segments[] = $query['rid'];
                     unset($query['rid']);
-                }
-                else
-                {
+                } else {
                     return array();
                 }
 
-                if (isset($query['rllabel']))
-                {
+                if (isset($query['rllabel'])) {
                     $segments[] = $query['rllabel'];
                     unset($query['rllabel']);
                 }
-            break;
-            default:;
-            break;
+
+                break;
+            default:
+                break;
         }
     }
 
@@ -179,7 +151,7 @@ function easyTableProBuildRoute(&$query)
  *
  * @since   1.0
  */
-function easyTableProParseRoute ($segments)
+function easyTableProParseRoute($segments)
 {
     $vars = array();
 
@@ -187,8 +159,7 @@ function easyTableProParseRoute ($segments)
     $count = count($segments);
 
     // Time to bail
-    if ($count == 0)
-    {
+    if ($count == 0) {
         $vars['view'] = 'tables';
 
         return $vars;
@@ -200,17 +171,15 @@ function easyTableProParseRoute ($segments)
     $menuItem   = $menu->getActive();
 
     // It's possible there's no active menu item... e.g. a search result link
-    if (!$menuItem)
-    {
+    if (!$menuItem) {
         $menuItem = $menu->getDefault();
     }
+
     // And we'll need the component params
-    $params = JComponentHelper::getParams('com_easytablepro');
     $db = JFactory::getDbo();
     $query = $db->getQuery(true);
 
-    if ($count == 1 && ($menuItem->query['view'] != 'records') )
-    {
+    if ($count == 1 && ($menuItem->query['view'] != 'records')) {
         $segments[0] = preg_replace('/:/', '-', $segments[0], 1);
         $vars['view'] = 'records';
 
@@ -224,20 +193,15 @@ function easyTableProParseRoute ($segments)
         $vars['id'] = $id;
 
         return $vars;
-    }
-    elseif ($count == 1 && ($menuItem->query['view'] == 'records') )
-    {
+    } elseif ($count == 1 && ($menuItem->query['view'] == 'records')) {
         $count = 2;
     }
 
     $vars['view'] = 'record';
 
-    if ($count == 2)
-    {
-        if (isset($menuItem->query['view']))
-        {
-            if ($menuItem->query['view'] == 'tables')
-            {
+    if ($count == 2) {
+        if (isset($menuItem->query['view'])) {
+            if ($menuItem->query['view'] == 'tables') {
                 // Remove the stupid colon
                 $segments[0] = preg_replace('/:/', '-', $segments[0], 1);
                 $alias = $segments[0];
@@ -249,25 +213,21 @@ function easyTableProParseRoute ($segments)
                 $vars['id'] = $id;
 
                 $rid = $segments[1];
-            }
-            elseif ($menuItem->query['view'] == 'records')
-            {
+            } elseif ($menuItem->query['view'] == 'records') {
                 // Convert the easy table alias to it actual id
-                if (isset($menuItem->query['id']))
-                {
+                if (isset($menuItem->query['id'])) {
                     $id = $menuItem->query['id'];
                     $vars['id'] = $id;
-                }
-                else
-                {
+                } else {
                     $vars['id'] = 0;
-                    $app->enqueueMessage(JText::_('COM_EASYTABLEPRO_SITE_ROUTER_PARSEROUTE_COULDNT_FIND_TABLE_ID'), 'Warning');
+                    $app->enqueueMessage(
+                        JText::_('COM_EASYTABLEPRO_SITE_ROUTER_PARSEROUTE_COULDNT_FIND_TABLE_ID'),
+                        'Warning'
+                    );
                 }
 
                 $rid = $segments[0];
-            }
-            elseif ($menuItem->query['view'] == 'record')
-            {
+            } elseif ($menuItem->query['view'] == 'record') {
                 $id = $segments[0];
                 $vars['id'] = $id;
                 $rid = $segments[1];
@@ -278,22 +238,17 @@ function easyTableProParseRoute ($segments)
     }
 
     // Three => id, rid, leaf
-    if ($count == 3)
-    {
+    if ($count == 3) {
         list($seg_id, $seg_alias) = explode(':', $segments[0], 2);
 
-        if (is_numeric($seg_id))
-        {
+        if (is_numeric($seg_id)) {
             $segments[0] = $seg_alias;
-        }
-        else
-        {
+        } else {
             $segments[0] = preg_replace('/:/', '-', $segments[0], 1);
         }
 
         // Convert the easy table alias to it actual id
-        if (!empty($segments[0]))
-        {
+        if (!empty($segments[0])) {
             $alias = $segments[0];
             $query->select($db->quoteName('id'));
             $query->from($db->quoteName('#__easytables'));
@@ -301,9 +256,7 @@ function easyTableProParseRoute ($segments)
             $db->setQuery($query);
             $id = $db->loadResult();
             $vars['id'] = $id;
-        }
-        else
-        {
+        } else {
             $vars['id'] = 0;
             $app->enqueueMessage(JText::_('COM_EASYTABLEPRO_SITE_ROUTER_PARSEROUTE_COULDNT_FIND_TABLE_ID'), 'Warning');
         }
